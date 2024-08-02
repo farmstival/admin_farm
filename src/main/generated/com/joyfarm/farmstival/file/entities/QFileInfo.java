477d80c1cd7fa173351720e0d1d279e9d6dcf0da
heyejiyang <xoxo99312@naver.com>
2024-08-02 14:27:33 +0900

    파일 공통 모듈 구성중

---
+++ src/main/generated/com/joyfarm/farmstival/file/entities/QFileInfo.java
@@ -0,0 +1,54 @@
+package com.joyfarm.farmstival.file.entities;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QFileInfo is a Querydsl query type for FileInfo
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFileInfo extends EntityPathBase<FileInfo> {
+
+    private static final long serialVersionUID = 1694653971L;
+
+    public static final QFileInfo fileInfo = new QFileInfo("fileInfo");
+
+    public final com.joyfarm.farmstival.global.entities.QBaseMemberEntity _super = new com.joyfarm.farmstival.global.entities.QBaseMemberEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> deletedAt = _super.deletedAt;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    //inherited
+    public final StringPath modifiedBy = _super.modifiedBy;
+
+    public final NumberPath<Long> seq = createNumber("seq", Long.class);
+
+    public QFileInfo(String variable) {
+        super(FileInfo.class, forVariable(variable));
+    }
+
+    public QFileInfo(Path<? extends FileInfo> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QFileInfo(PathMetadata metadata) {
+        super(FileInfo.class, metadata);
+    }
+
+}
+
