e93e821b9b3986f24dca0026357ffee54b547cfb
kth232 <kth7537@naver.com>
2024-08-08 19:21:05 +0900

    오류 해결 중

--- src/main/java/com/joyfarm/farmstival/tour/repositories/TourPlaceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.joyfarm.farmstival.tour.repositories;
-
-import com.joyfarm.farmstival.tour.entities.TourPlace;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface TourPlaceRepository extends JpaRepository<TourPlace, Long>, QuerydslPredicateExecutor<TourPlace> {
-}
